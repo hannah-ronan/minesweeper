59bcdb4613cfcdc129cc4376a36d611fef53c4e6
hannah-ronan <hannah.ronan6@gmail.com>
2021-03-04 23:52:40 -0500

    changed tile system to add tile class

--- game.gd
+++ game.gd
@@ -2,102 +2,117 @@ extends Node2D
 
 class_name game
 var tiles = []
-
-func create_grid(size):
+var tile = load("res://tile.gd")
+var size = 0
+func create_grid():
 	#create the game grid, filled only with 0s then populate it with mines
 	#the grid gets size*2 mines on it
-	for x in range(size):
+	for y in range(size):
 		tiles.append([])
-		for y in range(size):
-			tiles[x].append(0)
+		for x in range(size):
+			var new_tile = tile.new()
+			tiles[y].append(new_tile)
+			new_tile.x_loc = x
+			new_tile.y_loc = y
+			
 	var mine_count = 0
-	while mine_count<(size*2):
+	while mine_count<(size*3):
 		var randx = randi() % tiles.size()
 		var randy = randi() % tiles.size()
-		if tiles[randx][randy] != -1:
-			tiles[randx][randy] = -1
-			mine_count += 1
-	grid_numbers(size)
+		for row in tiles:
+			for tile in row:
+				if tile.x_loc == randx and tile.y_loc==randy:
+					tile.is_mine = true
+					mine_count+=1
+	#update_numbers()
 
-func grid_numbers(size):
-	for x in range (tiles.size()):
-		for y in range (tiles.size()):
-			#loop through all tiles in the grid to check how many mines are around them and update the tiles array 
-			if tiles[x][y] != -1:
-				#if the tile is not a mine
-				if x!=0 and x!=(size-1) and y!=0 and y!=(size-1):
-					#if the current tile is not on the edge of the grid
-					check_below_tiles(x,y)
-					check_beside_tiles(x,y)
-					check_above_tiles(x,y)
-				elif x==0:
-					#if the tile is in the top row
-					if y!=0 and y!=(size-1):
-						#if the tile is in the top row but not either corner
-						check_below_tiles(x,y)
-						check_beside_tiles(x,y)
-					else:
-						#if the tile is in one of the top corners
-						if y==0:
-							#if the tile is in the top left corner
-							check_tile(x,y,0,1)#check to the right
-							check_tile(x,y,1,0)#check to the bottom
-							check_tile(x,y,1,1)#check to the lower right
-						else:
-							#if the tile is in the top right corner
-							check_tile(x,y,0,-1)#check to the left
-							check_tile(x,y,1,0)#check to the bottom
-							check_tile(x,y,1,-1)#check to the lower left
-						
-				elif x==(size-1):
-					if y!=0 and y!=(size-1):
-						#if the tile is in the top row but not either corner
-						check_above_tiles(x,y)
-						check_beside_tiles(x,y)
-					else:
-						#if the tile is in one of the bottom corners
-						if y==0:
-							#if the tile is in the bottom left corner
-							check_tile(x,y,0,1)#check to the right
-							check_tile(x,y,-1,0)#check to the top
-							check_tile(x,y,-1,1)#check to the upper right
-						else:
-							#if the tile is in the bottom right corner
-							check_tile(x,y,0,-1)#check to the left
-							check_tile(x,y,-1,0)#check to the top
-							check_tile(x,y,-1,-1)#check to the upper left
-				elif y==0:
-					#if the tile is on the left edge
-					check_tile(x,y,-1,0)
-					check_tile(x,y,-1,1)
-					check_tile(x,y,0,1)
-					check_tile(x,y,1,1)
-					check_tile(x,y,1,0)
-				elif y==(size-1):
-					#if the tile is on the right edge
-					check_tile(x,y,-1,0)
-					check_tile(x,y,-1,-1)
-					check_tile(x,y,0,-1)
-					check_tile(x,y,1,-1)
-					check_tile(x,y,1,0)
-			
-func check_above_tiles(x,y):
-	#check if the 3 tiles above the current tile are mines
-	check_tile(x,y,-1,-1)
-	check_tile(x,y,-1,0)
-	check_tile(x,y,-1,1)
+func update_corners():
+	#define which tiles are corners 
+	for row in tiles:
+		for tile in row:
+			if tile.x_loc == 0 or tile.x_loc ==(size-1) or tile.y_loc == 0 or tile.y_loc ==(size-1):
+				tile.is_edge = true
+				if tile.y_loc == 0:
+					tile.edge_id += "top"
+				if tile.y_loc == (size-1):
+					tile.edge_id += "bottom"
+				if tile.x_loc == 0:
+					tile.edge_id += "left"
+				if tile.x_loc == (size-1):
+					tile.edge_id += "right"
+				
+				if (tile.x_loc==0 and tile.y_loc==0)or (tile.x_loc==0 and tile.y_loc==(size-1))or(tile.x_loc==(size-1) and tile.y_loc==0)or(tile.x_loc==(size-1) and tile.y_loc==(size-1)):
+					tile.is_corner = true
 
-func check_beside_tiles(x,y):
-	#check if the 2 tiles on either side of the current tile are mines
-	check_tile(x,y,0,-1)
-	check_tile(x,y,0,+1)
-
-func check_below_tiles(x,y):
-	#check if the 3 tiles below the current tile are mines
-	check_tile(x,y,1,-1)
-	check_tile(x,y,1,0)
-	check_tile(x,y,1,1)
+func update_numbers():
+	#loop through all tiles in the grid to check how many mines are around them and update the tiles array 
+	for row in tiles:
+		for tile in row:
+			if !tile.is_corner and !tile.is_edge:
+				check_all(tile)
+			elif tile.is_corner:
+				check_corner(tile)
+			elif tile.is_edge:
+				check_edge(tile)
+				
+func check_all(tile):
+	check_tile(tile, -1,-1)
+	check_tile(tile, 0,-1)
+	check_tile(tile, 1,-1)
+	check_tile(tile, -1,0)
+	check_tile(tile, 1,0)
+	check_tile(tile, -1,1)
+	check_tile(tile, 0,1)
+	check_tile(tile, 1,1)
 	
-func check_tile(x,y,xoffset, yoffset):
-	if tiles[x+xoffset][y+yoffset] == -1:
-		tiles[x][y]+=1
+func check_edge(tile):
+	if tile.edge_id=="right":
+		check_tile(tile, 0,-1)
+		check_tile(tile, -1,-1)
+		check_tile(tile, -1,0)
+		check_tile(tile, -1,1)
+		check_tile(tile, 0,1)
+		
+	if tile.edge_id=="left":
+		check_tile(tile, 0,-1)
+		check_tile(tile, 1,-1)
+		check_tile(tile, 1,0)
+		check_tile(tile, 1,1)
+		check_tile(tile, 0,1)
+		
+	if tile.edge_id=="top":
+		check_tile(tile, -1,0)
+		check_tile(tile, -1,1)
+		check_tile(tile, 0,1)
+		check_tile(tile, 1,1)
+		check_tile(tile, 1,0)
+		
+	if tile.edge_id=="bottom":
+		check_tile(tile, -1,0)
+		check_tile(tile, -1,-1)
+		check_tile(tile, 0,-1)
+		check_tile(tile, 1,-1)
+		check_tile(tile, 1,0)
+
+func check_corner(tile):
+	if tile.edge_id=="topright":
+		check_tile(tile, -1,0)
+		check_tile(tile, -1,1)
+		check_tile(tile, 0,1)
+	if tile.edge_id=="bottomright":
+		check_tile(tile, -1,0)
+		check_tile(tile, -1,-1)
+		check_tile(tile, 0,-1)
+	if tile.edge_id=="topleft":
+		check_tile(tile, 1,0)
+		check_tile(tile, 1,1)
+		check_tile(tile, 0,1)
+	if tile.edge_id=="bottomleft":
+		check_tile(tile, 0,-1)
+		check_tile(tile, 1,-1)
+		check_tile(tile, 1,0)
+	
+func check_tile(tile, xoffset, yoffset):
+	if tiles[tile.x_loc+xoffset][tile.y_loc+yoffset].is_mine:
+		tile.mine_count +=1
+	
--- main.gd
+++ main.gd
@@ -5,14 +5,17 @@ func on_ready():
 
 func _on_small_pressed():
 	var small_game = game.new()
-	small_game.create_grid(10)
+	small_game.size = 10
+	small_game.create_grid()
 
 
 func _on_medium_pressed():
 	var medium_game = game.new()
-	medium_game.create_grid(20)
+	medium_game.size = 20
+	medium_game.create_grid()
 
 
 func _on_large_pressed():
 	var large_game = game.new()
-	large_game.create_grid(30)
+	large_game.size = 30
+	large_game.create_grid()

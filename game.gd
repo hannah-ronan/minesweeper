ed8c1e37a9df1cf3b86b87190af065230e750923
hannah-ronan <hannah.ronan6@gmail.com>
2021-03-07 16:59:33 -0500

    commenting and adding flag instructions

--- game.gd
+++ game.gd
@@ -6,35 +6,11 @@ var tile_class = load("res://tile.tscn")
 var size = 10
 signal game_over
 signal game_won
-"""
-#debug tools, tile_clicked gets called whenever a tile is clicked, imagine that, and gets passed the tiles x and y in the tiles array
-func tile_clicked(x_loc, y_loc):
-	var adj_tiles = []
-	adj_tiles = add_adj_tiles(-1,-1, adj_tiles,x_loc, y_loc)
-	adj_tiles = add_adj_tiles(0,-1, adj_tiles,x_loc, y_loc)
-	adj_tiles = add_adj_tiles(1,-1, adj_tiles,x_loc, y_loc)
-	
-	adj_tiles = add_adj_tiles(-1,0, adj_tiles,x_loc, y_loc)
-	adj_tiles = add_adj_tiles(1,0, adj_tiles,x_loc, y_loc)
-	
-	adj_tiles = add_adj_tiles(-1,1, adj_tiles,x_loc, y_loc)
-	adj_tiles = add_adj_tiles(0,1, adj_tiles,x_loc, y_loc)
-	adj_tiles = add_adj_tiles(1,1, adj_tiles,x_loc, y_loc)
-	
-	for adj_tile in adj_tiles:
-		print (adj_tile.x_loc," ", adj_tile.y_loc," ",adj_tile.is_mine)
-	
-func add_adj_tiles(xoffset,yoffset, adj_tiles,x_loc, y_loc):
-	#checks to see if the tile exists and if it does then increment its minecount
-	if (x_loc+xoffset)>=0 and (y_loc+yoffset)>=0 and (x_loc+xoffset)<=(9) and (y_loc+yoffset)<=(9):
-		adj_tiles.append(tiles[y_loc+yoffset][x_loc+xoffset])
-	return adj_tiles
-"""
 
 	
 func create_grid():
 	#create the game grid, filled only with 0s then populate it with mines
-	#the grid gets size*2 mines on it
+	#the grid gets size*1.5 mines on it
 	randomize()#this is here so the board is different every time
 	for y in range(size):
 		tiles.append([])
@@ -43,12 +19,12 @@ func create_grid():
 			new_tile.x_loc = x
 			new_tile.y_loc = y
 			tiles[y].append(new_tile)
-			new_tile.set_name(str(x)+", "+str(y))
-			new_tile.connect("tile_clicked",self, "tile_clicked")
+			new_tile.set_name(str(x)+", "+str(y))#change the nodes name to be its location in the grid
+			new_tile.connect("tile_clicked",self, "tile_clicked")#connect the signal that gets sent from the tile object
 			
 	var mines_placed = 0
 	while mines_placed<(floor(size*1.5)):
-		#place 3X the size of the grid number of mines into the game
+		#place 1.5x the size of the grid number of mines into the game
 		var randx = randi() % size
 		var randy = randi() % size
 		for row in tiles:
@@ -56,7 +32,7 @@ func create_grid():
 				if tile.x_loc == randx and tile.y_loc==randy and tile.is_mine==false:
 					tile.is_mine = true
 					mines_placed+=1
-	mine_tally()
+	mine_tally()#update the mine_count of each tile then display this count in the game
 	for row in tiles:
 		for tile in row:
 			if !tile.is_mine:
@@ -90,12 +66,13 @@ func check_adj_tiles(tile):
 		tile_check("count_check",tile,1,1)
 	
 func tile_check(mode,tile,xoffset,yoffset):
-	#checks to see if the tile exists and if it does then increment its minecount
+	#checks to see if the tile exists
 	var new_x = tile.x_loc+xoffset
 	var new_y = tile.y_loc+yoffset
 	if (new_x)>=0 and (new_y)>=0 and (new_x)<=(size-1) and (new_y)<=(size-1):
 		#found the bug with mine_count being off... the tiles array is in the format tiles[y][x] not tiles[x][y] as I had before >:(
 		if mode == "count_check":
+			#if the tile and its adjacent tile is also equal to 0 then reveal it
 			if tile.mine_count==0:
 				if tiles[new_y][new_x].mine_count == tile.mine_count and !tiles[new_y][new_x].disabled:
 					tiles[new_y][new_x].disabled = true
@@ -104,6 +81,7 @@ func tile_check(mode,tile,xoffset,yoffset):
 					tiles[new_y][new_x].disabled = true
 			
 		if mode=="mine_check":
+			#id the adjacent tile is a mine then increment its minecount
 			if !tiles[new_y][new_x].is_mine:
 				tiles[new_y][new_x].mine_count += 1
 
@@ -117,6 +95,7 @@ func tile_clicked(x_loc,y_loc):
 		check_for_win()
 
 func check_for_win():
+	#if all the non-mine tiles are revealed then the game is won, send the signal out to main
 	var revealed_tiles = 0
 	var mines = 0
 	for row in tiles:
--- main.gd
+++ main.gd
@@ -4,56 +4,59 @@ onready var menu = get_node("main_menu")
 onready var quit_button = get_node("in_game_controls/quit_button")
 onready var lose_message = get_node("in_game_controls/loser")
 onready var win_message = get_node("in_game_controls/winner")
+onready var flag_instructions = get_node("flag_instructions")
 
 func _on_small_pressed():
+	#when the small size is chosen create a 10 by 10 grid and hide the main menu
 	menu.visible=false
 	var small_game = game_class.instance()
 	small_game.create_grid()
 	small_game.columns = 10
 	small_game.size = 10
 	get_node("center_container").add_child(small_game)
+	#connect the game won/lost signals 
 	get_node("center_container/game").connect("game_over",self, "game_over")
 	get_node("center_container/game").connect("game_won",self, "game_won")
 	draw_grid(small_game)
 	quit_button.disabled = false
+	flag_instructions.visible = true
 
 func _on_big_pressed():
+	#when the big size button is chosen create a 15x15 grid and hide the menu
 	menu.visible=false
 	var medium_game = game_class.instance()
 	medium_game.columns = 15
 	medium_game.size = 15
 	medium_game.create_grid()
 	get_node("center_container").add_child(medium_game)
+	#connect the game won/lost signals from game
 	get_node("center_container/game").connect("game_over",self, "game_over")
 	get_node("center_container/game").connect("game_won",self, "game_won")
 	draw_grid(medium_game)
 	quit_button.disabled = false
+	flag_instructions.visible = true
 
-"""
-func _on_large_pressed():
-	menu.visible=false
-	var large_game = game_class.instance()
-	large_game.columns = 20
-	large_game.size = 20
-	large_game.create_grid()
-	get_node("center_container").add_child(large_game)
-	draw_grid(large_game)
-	quit_button.disabled = false
-"""
 	
 func draw_grid(grid):
+	#add the tiles to the gridcontainer
 	for row in grid.tiles:
 		for tile in row:
 			grid.add_child(tile)
 
 func _on_quit_button_pressed():
+	#when quit is pressed delete all the tiles then the grid
+	for child in get_node("center_container/game").get_children():
+		child.queue_free()
 	get_node("center_container/game").queue_free()
+	#show the menu and get rid of the quit button and win/lose messages
 	menu.visible = true
 	quit_button.disabled = true
 	lose_message.visible = false
 	win_message.visible = false
+	flag_instructions.visible = false
 
 func game_over(game_instance):
+	#called when a mine is clicked, show the whole grid and set the mine text color to red
 	for row in game_instance.tiles:
 		for tile in row:
 			if tile.is_mine:
@@ -62,6 +65,7 @@ func game_over(game_instance):
 	lose_message.visible = true
 
 func game_won(game_instance):
+	#called when the game is won, show the whole grid and make the mines red
 	for row in game_instance.tiles:
 		for tile in row:
 			if tile.is_mine:
